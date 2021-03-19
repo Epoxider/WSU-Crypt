03e4f8eb56226f1e7334f59275bd31bc976fdd36
Matthew Fritz <matthew.r.fritz@gmail.com>
2021-03-18 20:19:40 -0700

    save 2

--- main.c
+++ main.c
@@ -4,26 +4,53 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-struct TValues {
-    unsigned short t0;
-    unsigned short t1;
-};
+ftable[] = {0xa3,0xd7,0x09,0x83,0xf8,0x48,0xf6,0xf4,0xb3,0x21,0x15,0x78,0x99,0xb1,0xaf,0xf9,0xe7,0x2d,0x4d,0x8a,0xce,0x4c,0xca,0x2e,0x52,0x95,0xd9,0x1e,0x4e,0x38,0x44,0x28,0x0a,0xdf,0x02,0xa0,0x17,0xf1,0x60,0x68,0x12,0xb7,0x7a,0xc3,0xe9,0xfa,0x3d,0x53,0x96,0x84,0x6b,0xba,0xf2,0x63,0x9a,0x19,0x7c,0xae,0xe5,0xf5,0xf7,0x16,0x6a,0xa2,0x39,0xb6,0x7b,0x0f,0xc1,0x93,0x81,0x1b,0xee,0xb4,0x1a,0xea,0xd0,0x91,0x2f,0xb8,0x55,0xb9,0xda,0x85,0x3f,0x41,0xbf,0xe0,0x5a,0x58,0x80,0x5f,0x66,0x0b,0xd8,0x90,0x35,0xd5,0xc0,0xa7,0x33,0x06,0x65,0x69,0x45,0x00,0x94,0x56,0x6d,0x98,0x9b,0x76,0x97,0xfc,0xb2,0xc2,0xb0,0xfe,0xdb,0x20,0xe1,0xeb,0xd6,0xe4,0xdd,0x47,0x4a,0x1d,0x42,0xed,0x9e,0x6e,0x49,0x3c,0xcd,0x43,0x27,0xd2,0x07,0xd4,0xde,0xc7,0x67,0x18,0x89,0xcb,0x30,0x1f,0x8d,0xc6,0x8f,0xaa,0xc8,0x74,0xdc,0xc9,0x5d,0x5c,0x31,0xa4,0x70,0x88,0x61,0x2c,0x9f,0x0d,0x2b,0x87,0x50,0x82,0x54,0x64,0x26,0x7d,0x03,0x40,0x34,0x4b,0x1c,0x73,0xd1,0xc4,0xfd,0x3b,0xcc,0xfb,0x7f,0xab,0xe6,0x3e,0x5b,0xa5,0xad,0x04,0x23,0x9c,0x14,0x51,0x22,0xf0,0x29,0x79,0x71,0x7e,0xff,0x8c,0x0e,0xe2,0x0c,0xef,0xbc,0x72,0x75,0x6f,0x37,0xa1,0xec,0xd3,0x8e,0x62,0x8b,0x86,0x10,0xe8,0x08,0x77,0x11,0xbe,0x92,0x4f,0x24,0xc5,0x32,0x36,0x9d,0xcf,0xf3,0xa6,0xbb,0xac,0x5e,0x6c,0xa9,0x13,0x57,0x25,0xb5,0xe3,0xbd,0xa8,0x3a,0x01,0x05,0x59,0x2a,0x46};
 
-unsigned short TValues G(unsigned short w, short round) {
+unsigned char FTable(unsigned char val) {
+    unsigned char row = (val >> 4) & 0x0f;
+    unsigned char col = val &0x0f;
+    return ftable[row*16 + col];
+}
+
+unsigned char K_Encrypt(unsigned long x, unsigned long * keyPtr) {
+    *keyPtr = (*keyPtr << 1)|(*keyPtr >> 63);
+    unsigned long whichByte = x%8;
+    unsigned long result = (*keyPtr >> (whichByte * 8)) & 0xff; 
+    return (unsigned char) (result & 0xff);
+}
+
+int K_Decrypt(unsigned long subRound, unsigned long * keyPtr) {
+    unsigned long key = &keyPtr;
+
+    return subRound;
+}
+
+unsigned short G_Encrypt(unsigned short w, unsigned long round, unsigned long * keyPtr) {
+    unsigned char g1 = (w >> 8) &0xff;
+    unsigned char g2 = w & 0xff;
+    unsigned char g3, g4, g5, g6;
+
+    g3 = FTable(g2^K_Encrypt(4*round, keyPtr))^g1;
+    g4 = FTable(g3^K_Encrypt(4*round + 1, keyPtr))^g2;
+    g5 = FTable(g4^K_Encrypt(4*round + 2, keyPtr))^g3;
+    g6 = FTable(g5^K_Encrypt(4*round + 3, keyPtr))^g4;
+
+    return ((unsigned short) (g5)) << 8 | (unsigned short) g6;
 
 }
 
-void funcF(unsigned short r0, unsigned short r1, short round) {
+void funcF(unsigned short r0, unsigned short r1, unsigned long round) {
 
-    struct TValues tvals;
-    tvals.t0 = G(r0, round);
-    tvals.t1 = G(r1, round);
-    //unsigned short t0 = G(r0, round);
-    //unsigned short t1 = G(r0, round);
+    unsigned short t0 = G(r0, round);
+    unsigned short t1 = G(r1, round);
 
 }
 
-void whitening(unsigned char* block, unsigned char * keyPtr) {
+void encrypt(unsigned char * block, unsigned long * keyPtr) {
+    int i = 0;
+    unsigned long round = 0;
+
+    //whitening
     unsigned short w0 = ( ((unsigned short) block[0]) << 8) + block[1];
     unsigned short w1 = ( ((unsigned short) block[2]) << 8) + block[3];
     unsigned short w2 = ( ((unsigned short) block[4]) << 8) + block[5];
@@ -38,28 +65,11 @@ void whitening(unsigned char* block, unsigned char * keyPtr) {
     unsigned short r1 = w1^k1;
     unsigned short r2 = w2^k2;
     unsigned short r3 = w3^k3;
-}
 
-void encrypt(unsigned char * block, unsigned char * keyPtr) {
-    short round = 0;
-
-    //whitening(block, keyPtr);
-    unsigned short w0 = ( ((unsigned short) block[0]) << 8) + block[1];
-    unsigned short w1 = ( ((unsigned short) block[2]) << 8) + block[3];
-    unsigned short w2 = ( ((unsigned short) block[4]) << 8) + block[5];
-    unsigned short w3 = ( ((unsigned short) block[6]) << 8) + block[7];
-
-    unsigned short k0 = ( ((unsigned short) keyPtr[0]) << 8) + keyPtr[1];
-    unsigned short k1 = ( ((unsigned short) keyPtr[2]) << 8) + keyPtr[3];
-    unsigned short k2 = ( ((unsigned short) keyPtr[4]) << 8) + keyPtr[5];
-    unsigned short k3 = ( ((unsigned short) keyPtr[6]) << 8) + keyPtr[7];
-
-    unsigned short r0 = w0^k0;
-    unsigned short r1 = w1^k1;
-    unsigned short r2 = w2^k2;
-    unsigned short r3 = w3^k3;
-
-    funcF(r0, r1, round);
+    for(i; i <= 15; i++) {
+        funcF(r0, r1, round);
+        round++;
+    }
 }
 
 void decrypt() {
@@ -68,7 +78,8 @@ void decrypt() {
 
 int main(int argc, char* argv[]) {
 	FILE *keyFilePtr, *inputFilePtr, *outputFilePtr;
-    unsigned char * keyPtr, block[9];
+    unsigned char block[9];
+    unsigned long key;
     int i, keySize, readSize;
     bool isEndcode;
 
@@ -90,12 +101,8 @@ int main(int argc, char* argv[]) {
         printf("key file is not 64 bits");
         exit(1);
     }
-    keyPtr = (unsigned char *) malloc(keySize * sizeof(unsigned char));
-    if (keyPtr == NULL) {
-        printf("could not allocate key file");
-        exit(1);
-    }
-    readSize = fread(keyPtr, 1, keySize, keyFilePtr);
+
+    readSize = fread(&key, 1, keySize, keyFilePtr);
     if (readSize != keySize) {
         printf("Mismatch in key file size");
         exit(1);
@@ -113,12 +120,10 @@ int main(int argc, char* argv[]) {
             }
         }
         readSize = fread(block, 1, 8, inputFilePtr);
-        encrypt(block, keyPtr);
+        encrypt(block, &key);
 
     }
 
-    free(keyPtr);
-    keyPtr = NULL;
     fclose(inputFilePtr);
 	exit(0);
 }
